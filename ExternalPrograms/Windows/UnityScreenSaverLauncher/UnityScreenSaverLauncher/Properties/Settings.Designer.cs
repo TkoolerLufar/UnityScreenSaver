4d69bc499ea200d9898550a1278d65fc7829b205
TkoolerLufar <19635219+TkoolerLufar@users.noreply.github.com>
2023-12-16 05:09:51 +0900

    Update launcher for Windows

--- ExternalPrograms/Windows/UnityScreenSaverLauncher/UnityScreenSaverLauncher/Properties/Settings.Designer.cs
+++ ExternalPrograms/Windows/UnityScreenSaverLauncher/UnityScreenSaverLauncher/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace UnityScreenSaverLauncher.Properties {
+namespace ScreenSaverByUnity.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
